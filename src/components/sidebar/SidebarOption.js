63625ef26244090eab818bb018d396a8f558ef65
youhei usami <usao9110@icloud.com>
2026-01-10 10:10:58 +0400

    1/10

---
+++ src/components/sidebar/SidebarOption.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SidebarOption = ({ text, Icon }) => {
+  return (
+    <div>
+      <Icon/>
+      <h2>{text}</h2>
+    </div>
+  )
+}
+
+export default SidebarOption
