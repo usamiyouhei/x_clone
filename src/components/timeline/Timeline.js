cdd507cbc60b157e0e047aa7043c2e572adc55f3
youhei usami <usao9110@icloud.com>
2026-01-12 12:23:03 +0400

    1/12

---
+++ src/components/timeline/Timeline.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Timeline() {
+  return (
+    <div className="timeline">
+      {/* Header */}
+      <div className="timeline--header">
+        <h2></h2>
+
+      </div>
+
+
+      {/* TweetBox */}
+
+      {/* Post */}
+    </div>
+  )
+}
+
+export default Timeline
